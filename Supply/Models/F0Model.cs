deac7ab24b59c0654f58bd9379629752a84dc3f5
Sokolov <sokolov_ea@farmsib.ru>
2019-01-08 16:30:07 +0300

    q

--- Supply/Models/F0Model.cs
+++ Supply/Models/F0Model.cs
@@ -5,6 +5,34 @@ namespace FNet.Supply.Models
     public class F0Model
     {
         public Guid SessionId;
-        public F0Model(Guid sessionId) { SessionId = sessionId; }
+        public FilterData Filter;
+        public FilteredData Data;
+
+        public F0Model(Guid sessionId)
+        {
+            SessionId = sessionId;
+            Filter = new FilterData(); // зависит от SessionId
+            Data = new FilteredData();// зависит от Filter
+        }
+        public class FilterData
+        {
+            public String все;
+            // ather fields ...
+            public FilterData()
+            {
+                // todo: Filter - загрузить или создать по умолчанию.
+                все = "False"; // фильтр по полю "обработано". По умолчанию обработанные строки не показываем.
+                // ather field defaults ...
+            }
+        }
+        public class FilteredData
+        {
+            // todo: Data - загрузить или создать по умолчанию.
+            //public fields ...
+            public FilteredData()
+            {
+                // field defaults ...
+            }
+        }
     }
 }
